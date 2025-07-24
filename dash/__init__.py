e13bf1e548c2e3ce7e74f8595ab23a57ad805a25
Lekuru <contact@lekuru.xyz>
2025-07-24 23:28:40 +0200

    Implement exception handlers

--- dash/__init__.py
+++ dash/__init__.py
@@ -1,6 +1,7 @@
 
 from . import logging
 from . import server
+from . import errors
 from . import state
 
 logger = logging.getLogger('dash')
--- dash/errors.py
+++ dash/errors.py
@@ -1,2 +1,30 @@
 
-# TODO: i18n error responses
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import ResponseValidationError
+from fastapi.responses import JSONResponse
+from fastapi import Request, HTTPException
+from .server import api
+
+@api.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        {'error': exc.status_code, 'message': exc.detail},
+        status_code=exc.status_code
+    )
+
+@api.exception_handler(StarletteHTTPException)
+async def starlette_exception_handler(request: Request, exc: StarletteHTTPException):
+    return JSONResponse(
+        {'error': exc.status_code, 'message': exc.detail},
+        status_code=exc.status_code
+    )
+
+@api.exception_handler(ResponseValidationError)
+async def response_validation_exception_handler(request: Request, exc: ResponseValidationError):
+    if api.debug:
+        raise exc
+
+    return JSONResponse(
+        {'error': 500, 'message': 'Internal Server Error'},
+        status_code=500
+    )
