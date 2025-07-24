39073acbbd0f9f59bd0fec4175b8c19626492485
Lekuru <contact@lekuru.xyz>
2025-07-24 23:14:20 +0200

    Implement vanilla activation endpoint

--- dash/routes/services/activate/vanilla.py
+++ dash/routes/services/activate/vanilla.py
@@ -1,4 +1,87 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, Request, APIRouter, Depends, Form
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from types import ModuleType
+
+from dash.data.repositories import activations, penguins
+from dash.data import Language
+from dash import state
+
+import i18n
 
 router = APIRouter()
+
+@router.post("/{language}")
+def vanilla_activation(
+    request: Request,
+    language: Language,
+    database: Session = Depends(state.database_session),
+    config: ModuleType = Depends(state.config),
+    username: str = Form(..., alias="name"),
+    activation_key: str = Form(..., alias="activationcode"),
+    recaptcha_response: str | None = Form(None, alias="recaptcha_response"),
+) -> RedirectResponse:
+    if not validate_recaptcha(recaptcha_response, config):
+        raise HTTPException(
+            status_code=400,
+            detail=i18n.t("activate.captcha_invalid", locale=language.value)
+        )
+
+    if not (penguin := penguins.fetch_by_name(username, database)):
+        raise HTTPException(
+            status_code=404,
+            detail=i18n.t("activate.username_404", locale=language.value)
+        )
+
+    if not (activation := activations.fetch_one(activation_key, database)):
+        raise HTTPException(
+            status_code=404,
+            detail=i18n.t("activate.activation_key_404", locale=language.value)
+        )
+
+    if penguin.id != activation.penguin_id:
+        raise HTTPException(
+            status_code=403,
+            detail=i18n.t("activate.incorrect_username", locale=language.value)
+        )
+
+    was_updated = penguins.update(
+        penguin.id,
+        {'active': True},
+        session=database
+    )
+
+    if not was_updated:
+        raise HTTPException(
+            status_code=500,
+            detail=i18n.t("activate.error", locale=language.value)
+        )
+
+    activations.delete(activation_key, database)
+    return RedirectResponse(config.VANILLA_ACTIVATE_REDIRECT)
+
+def validate_recaptcha(recaptcha_response: str | None, config: ModuleType) -> bool:
+    if not config.GSECRET_KEY:
+        # Recaptcha is not configured
+        return True
+
+    if not recaptcha_response:
+        # No recaptcha response provided
+        return False
+
+    requests = state.requests()
+    response = requests.post(
+        config.GCAPTCHA_URL,
+        data={
+            'secret': config.GSECRET_KEY,
+            'response': recaptcha_response
+            # TODO: Add remote IP
+        }
+    )
+
+    if response.ok:
+        return False
+
+    data = response.json()
+    return data.get('success', False)
