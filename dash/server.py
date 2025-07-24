c4ca82179f7d5ef54e88bba0d31ba22ad307adc1
Lekuru <contact@lekuru.xyz>
2025-07-24 23:14:09 +0200

    Implement i18n localisation

--- dash/server.py
+++ dash/server.py
@@ -4,12 +4,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dash import state
 
-import warnings
+import i18n
 import os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    locale_path = os.environ.get('DASH_LOCALE_PATH', './dash/data/locale')
     config_path = os.environ.get('DASH_CONFIG', 'config.py')
+    i18n.load_path.append(locale_path)
     state.initialize(config_path)
     state.setup_logging()
     yield
