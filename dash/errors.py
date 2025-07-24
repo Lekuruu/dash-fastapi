0e4b8eb3d22f00d0e58b38fea8ae896c7e6d4b4b
Lekuru <contact@lekuru.xyz>
2025-07-24 23:44:18 +0200

    Add request validation error handler

--- dash/errors.py
+++ dash/errors.py
@@ -1,6 +1,6 @@
 
+from fastapi.exceptions import ResponseValidationError, RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from fastapi.exceptions import ResponseValidationError
 from fastapi.responses import JSONResponse
 from fastapi import Request, HTTPException
 from .server import api
@@ -17,6 +17,13 @@ async def starlette_exception_handler(request: Request, exc: StarletteHTTPExcept
     return JSONResponse(
         {'error': exc.status_code, 'message': exc.detail},
         status_code=exc.status_code
+    )
+
+@api.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        {'error': 400, 'message': 'Bad Request', 'details': exc.errors()},
+        status_code=400
     )
 
 @api.exception_handler(ResponseValidationError)
